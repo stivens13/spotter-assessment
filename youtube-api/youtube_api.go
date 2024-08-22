9f891c2113a82477a7ade62e816a3498f1311f94
Nikita Voloshenko <stivens54@gmail.com>
2024-08-22 05:45:03 -0700

    add youtube-api mock

---
+++ youtube-api/youtube_api.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stivens13/spotter-assessment/tools/generator"
+)
+
+type YoutubeHandler struct {
+}
+
+func NewYoutubeHandler(
+	e *echo.Echo,
+) *YoutubeHandler {
+	youtubeHandler := &YoutubeHandler{
+	}
+
+	e.GET("/api/:channel_id", youtubeHandler.FetchVideoMetadataByChannel)
+	return youtubeHandler
+}
+
+
+func (yh *YoutubeHandler) FetchVideoMetadataByChannel(c echo.Context) error {
+	channelID := c.Param("channel_id")
+	videos := generator.GenerateMockVideo(channelID)
+	return c.JSON(200, videos)
+}
+
+type Services struct {
+	Server *echo.Echo
+}
+
+func InitServices() *Services {
+	echo := echo.New()
+	return &Services{
+		Server: echo,
+	}
+}
+
+func main() {
+	fmt.Println("Youtube Mock API starting...")
+	services := InitServices()
+	fmt.Println("Services successfully initialized")
+	server := services.Server
+	fmt.Println("Starting server")
+	server.Logger.Fatal(server.Start(":9000"))
+}
